22ee432156f21432b141c127c335bde3bf137d3e
tomishima <pumoco2904@icloud.com>
2022-10-09 11:58:17 +0900

    本番用の趣味dbの仕様をmodelsに記載

--- hobby/models.py
+++ hobby/models.py
@@ -10,3 +10,15 @@ class HobbiesTmp(models.Model):
     class Meta:
         managed = False
         db_table = 'hobbies_tmp'
+
+# 本番用のテーブル
+class Hobbies(models.Model):
+    hobby = models.CharField(max_length=30)
+    outdoor = models.IntegerField(blank=True, null=True)
+    skill = models.IntegerField(blank=True, null=True)
+    team = models.IntegerField(blank=True, null=True)
+    cost = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'hobbies'
